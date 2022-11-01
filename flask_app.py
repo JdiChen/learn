2584e4f80402b83ba688316f727e1b05dd38b09f
Fang_Yue <48125566+JdiChen@users.noreply.github.com>
2022-11-01 16:22:22 +0800

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,37 +1,64 @@
-from flask import Flask, redirect, url_for, request, render_template,abort
+from flask import Flask, redirect, url_for, request, render_template, abort
 
 app = Flask(__name__)
 app.debug = True
+USER = {
+    "user_name": "",
+    "age": "",
+    "phone": ""
+}
 
-def fail(fail_mesg):
+
+def fail(fail_mesg, err_code=500, return_obj={}):
     fail = {
-        "code": 500,
-        "error_mesg": fail_mesg
+        "code": err_code,
+        "message": fail_mesg,
+        "return_obj": return_obj
     }
     return fail
 
 
-def success(mesg):
+def success(mesg, return_obj={}):
     done = {
         "code": 200,
-        "message": f"success get {mesg}"
+        "message": f"success {mesg}",
+        "return_obj": return_obj
     }
     return done
 
 
-@app.route('/test', methods=["POST"])
-def test():
-    name = request.json['myname']
-    if name == "cq":
-        abort(404,fail("not allow!!"))
+@app.route('/addUser', methods=["POST"])
+def add_user():
+    global USER
+    user_info = request.json
+    if not isinstance(user_info['phone'], str):
         return fail("not allow!!")
-    return success(name)
+    USER['user_name'] = user_info['user_name']
+    USER['age'] = user_info["age"]
+    USER['phone'] = user_info["phone"]
+    return success("add user", USER)
 
 
-@app.route("/var/<string:show>/")
-def get_var(show):
-    return "get " + show
+@app.route("/findUser/<string:name>")
+def get_user_info(name):
+    global USER
+    user_info = {
+        "mesg": "not people"
+    }
+    if name == USER['user_name']:
+        user_info = {
+            "user_name": USER['user_name'],
+            "age": USER['age'],
+            "phone": USER["phone"]
+        }
+    return success("", user_info)
+
+
+@app.route("/delUser", methods=["POST"])
+def del_user():
+    name = request.json['user_name']
+    return success(f"del {name} done")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=65525)
